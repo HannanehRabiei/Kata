eb62e955a210737213970dab65d8d23f92d6a4e6
Hannaneh Rabiei <hannanehrabiei82@gmail.com>
2023-02-17 17:05:59 +0330

    new kata

---
+++ src/main/java/codewars/Dinglemouse7.java
@@ -0,0 +1,59 @@
+package codewars;
+
+public class Dinglemouse7 {
+
+    public int[] ownedCatAndDog(final int catYears, final int dogYears) {
+
+        int humanOfCat = calculateAgeOfHumanBasedAgeCat(catYears);
+        int humanOfDog = calculateAgeOfHumanBasedAgeDog(dogYears);
+
+        return new int[]{humanOfCat,humanOfDog};
+
+    }
+
+    private  int calculateAgeOfHumanBasedAgeCat(int catAge)
+    {
+
+                if (catAge<15)
+                    return 0;
+
+                if (catAge<24)
+                    return 1;
+
+                if (catAge<28)
+                    return 2;
+
+                if (catAge<32)
+                    return 3;
+
+                if (catAge<36)
+                    return 4;
+
+                if (catAge<40)
+                  return 5;
+
+                return 0;
+    }
+    private  int calculateAgeOfHumanBasedAgeDog(int dogAge) {
+
+        if (dogAge<15)
+            return 0;
+
+        if (dogAge<24)
+            return 1;
+
+        if (dogAge<29)
+            return 2;
+
+        if (dogAge<34)
+            return 3;
+
+        if (dogAge<39)
+            return 4;
+
+        if (dogAge<44)
+            return 5;
+
+        return 0;
+    }
+}
