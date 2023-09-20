8dce9b6b498f2c0f6dc1cdc0c7a31dca5b3b8733
Hannaneh Rabiei <hannanehrabiei82@gmail.com>
2023-09-20 12:07:21 +0330

    add new kata

---
+++ src/main/java/codewars/Kata.java
@@ -0,0 +1,24 @@
+package codewars;
+
+public class Kata {
+    public  int dontGiveMeFive(int start, int end) {
+        int count = 0;
+        for (int index = start; index <= end; index++) {
+            if (!hasFive(index))
+                count++;
+        }
+        return count;
+
+    }
+
+    private boolean hasFive(int number) {
+        number = Math.abs(number);
+        while (number > 0) {
+            if (number % 10 == 5)
+                return true;
+
+            number = number / 10;
+        }
+        return false;
+    }
+}
